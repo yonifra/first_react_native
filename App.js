52c620b57228a456bf771e44144a6155e22da511
Jonathan Fraimorice <jonathanfr@wix.com>
2020-05-14 00:53:18 +0300

    first working component!

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello, world!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello, world!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    backgroundColor: 'teal',
+    marginTop: 45,
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
 
 export default App;
